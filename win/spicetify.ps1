8a4f3f6ae686fe98fca5834e90d2c84772908da1
evgeniy.gleba <i@egleba.ru>
2025-10-05 22:48:20 +0300

    small fixes

--- win/spicetify.ps1
+++ win/spicetify.ps1
@@ -1,1 +1,10 @@
-iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$url = 'https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1'
+$dst = Join-Path $env:TEMP 'spicetify-marketplace-install.ps1'
+
+Invoke-WebRequest -Uri $url -OutFile $dst -UseBasicParsing
+Unblock-File $dst
+& $dst
+Remove-Item -Force $dst
